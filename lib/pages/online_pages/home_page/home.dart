7a7ce60e95543ea37778b1a4bafbb2334c5bc663
Grimmmm0 <boneclinkz35@gmail.com>
2024-11-05 15:32:27 +0800

    Modules added and Rerouted Online Pages

--- lib/pages/online_pages/home_page/home.dart
+++ lib/pages/online_pages/home_page/home.dart
@@ -1,14 +1,11 @@
-// home.dart
-
-// ignore_for_file: avoid_unnecessary_containers, sort_child_properties_last, prefer_const_literals_to_create_immutables, prefer_const_constructors
-
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_1_page/OneSubModule.dart';
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_1_page/level_1_categories_pages/commonwords_page/commonwords.dart';
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_2_page/TwoSubModule.dart';
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_3_page/ThreeSubModule.dart';
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_4_page/FourSubModule.dart';
 import 'package:flutter/material.dart';
-import 'package:ekumpas_beta/pages/online_pages/categories_pages/alphabet_page/alphabet.dart';
-import 'package:ekumpas_beta/pages/online_pages/categories_pages/commonwords_page/commonwords.dart';
-import 'package:ekumpas_beta/pages/online_pages/categories_pages//greetings_page/greetings.dart';
-import 'package:ekumpas_beta/pages/online_pages/categories_pages//questions_page/questions.dart';
-import 'package:ekumpas_beta/pages/online_pages/categories_pages/survivalsigns_page/survivalsigns.dart';
 import 'package:ekumpas_beta/provider/provider.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
@@ -26,7 +23,7 @@ class Home extends StatelessWidget {
             ),
           ),
           title: Text(
-            "Menu",
+            "Modules",
             style: TextStyle(
               color: notifier.isDark ? Colors.white : Colors.black,
             ),
@@ -34,178 +31,93 @@ class Home extends StatelessWidget {
           centerTitle: true,
         ),
         body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            child: GridView(
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AlphabetPage(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color.fromARGB(255, 18, 22, 60),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.abc_rounded,
-                          size: 80,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          "Alphabet",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CommonWordsPage(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color.fromARGB(255, 18, 22, 60),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.chat_rounded,
-                          size: 60,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          "Common",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Text(
-                          "Words",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => QuestionsPage(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color.fromARGB(255, 18, 22, 60),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.question_mark,
-                          size: 80,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          "Questions",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => GreetingsPage(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color.fromARGB(255, 18, 22, 60),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.safety_divider_rounded,
-                          size: 80,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          "Greetings",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SurvivalSignsPage(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color.fromARGB(255, 18, 22, 60),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.safety_check_rounded,
-                          size: 60,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          "Survival",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Text(
-                          "Signs",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 40,
-                crossAxisSpacing: 40,
-              ),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+          child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+              childAspectRatio: 2,
             ),
+            children: [
+              _buildMenuItem(
+                context,
+                icon: LucideIcons.handMetal,
+                label: "Level 1",
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const OneSubModulePage()),
+                ),
+              ),
+              _buildMenuItem(
+                context,
+                icon: LucideIcons.handHelping,
+                label: "Level 2",
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const TwoSubModulePage()),
+                ),
+              ),
+              _buildMenuItem(
+                context,
+                icon: LucideIcons.handHeart,
+                label: "Level 3",
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ThreeSubModulePage()),
+                ),
+              ),
+              _buildMenuItem(
+                context,
+                icon: LucideIcons.heartHandshake,
+                label: "Level 4",
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const FourSubModulePage()),
+                ),
+              ),
+            ],
           ),
         ),
       );
     });
+  }
+
+  Widget _buildMenuItem(BuildContext context,
+      {required IconData icon,
+      required String label,
+      required VoidCallback onTap}) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        constraints: BoxConstraints(
+          minWidth: MediaQuery.of(context).size.width * 0.4,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: const Color.fromARGB(255, 18, 22, 60),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 60, color: Colors.white),
+            const SizedBox(height: 8),
+            Text(
+              label,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/online_pages/module_pages/level_1_page/OneSubModule.dart
@@ -0,0 +1,209 @@
+// ignore_for_file: avoid_unnecessary_containers, sort_child_properties_last
+
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_1_page/level_1_categories_pages/alphabet_page/alphabet.dart';
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_1_page/level_1_categories_pages/commonwords_page/commonwords.dart';
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_1_page/level_1_categories_pages/greetings_page/greetings.dart';
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_1_page/level_1_categories_pages/questions_page/questions.dart';
+import 'package:ekumpas_beta/pages/online_pages/module_pages/level_1_page/level_1_categories_pages/survivalsigns_page/survivalsigns.dart';
+import 'package:flutter/material.dart';
+import 'package:ekumpas_beta/provider/provider.dart';
+import 'package:provider/provider.dart';
+
+class OneSubModulePage extends StatelessWidget {
+  const OneSubModulePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<UiProvider>(builder: (context, UiProvider notifier, child) {
+      return Scaffold(
+        appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
+          ),
+          title: Text(
+            "Level 1",
+            style: TextStyle(
+              color: notifier.isDark ? Colors.white : Colors.black,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            child: GridView(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AlphabetPage(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 18, 22, 60),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.abc_rounded,
+                          size: 80,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "Alphabet",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CommonWordsPage(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 18, 22, 60),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.chat_rounded,
+                          size: 60,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "Common",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          "Words",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const QuestionsPage(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 18, 22, 60),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.question_mark,
+                          size: 80,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "Questions",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const GreetingsPage(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 18, 22, 60),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.safety_divider_rounded,
+                          size: 80,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "Greetings",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SurvivalSignsPage(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color.fromARGB(255, 18, 22, 60),
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.safety_check_rounded,
+                          size: 60,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "Survival",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          "Signs",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 40,
+                crossAxisSpacing: 40,
+              ),
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
